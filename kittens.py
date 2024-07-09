6971bb34570f6f287ca5339ec45d7338f7b34e5a
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2024-07-08 20:13:55 -0700

    initial commit

---
+++ kittens.py
@@ -0,0 +1,10 @@
+kittens = ['suki','pusheen','hello kitty']
+
+# print(kittens)
+
+def countoffkittens(kittens):
+    print('We found' , len(kittens) , 'kittens!')
+    print('and their names are')
+    for kitten in kittens:
+        print(kitten)
+countoffkittens(kittens)
